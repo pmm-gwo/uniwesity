1daf1300a6128415058b763e6458d498658107eb
lombek <lombek@gmail.com>
2022-04-24 12:59:12 +0200

    Instrukcja 2 - zadanie 1

---
+++ src/Administrator.java
@@ -0,0 +1,12 @@
+public class Administrator extends User {
+    public Administrator(String firstName, String lastName,
+                         String email, String password) {
+        super(firstName, lastName, email, password);
+    }
+
+    @Override
+    public String toString() {
+        return "Administrator";
+    }
+
+}
--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,7 @@
 public class Runner {
 
     public static void main(String[] args) {
-//        test();
+        test();
         LoginForm loginForm = new LoginForm();
         loginForm.initialize();
     }
@@ -11,11 +11,14 @@ public class Runner {
                 "janek342@gmail.com", "Jan@13", 344421L);
         Teacher teacher1 = new Teacher("Małgorzata", "Kowalska",
                 "kowalska@gmail.com", "malg@onE", "mgr");
+        Administrator administrator1 = new Administrator("Witold", "Kowalski",
+                "witek35@gmail.com", "Qwert@e9x3");
 
-//        FileUserRepository fileUserRepository = new FileUserRepository();
-//        fileUserRepository.insert(student1);
-//        fileUserRepository.insert(teacher1);
-//        System.out.println(fileUserRepository.findAll());
+        FileUserRepository fileUserRepository = new FileUserRepository();
+        fileUserRepository.insert(student1);
+        fileUserRepository.insert(teacher1);
+        fileUserRepository.insert(administrator1);
+        System.out.println(fileUserRepository.findAll());
     }
 
 }
